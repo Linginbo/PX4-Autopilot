7ea4eb83c7efdb9ff1c63338bc8b0ac0cdfa4a4a
Matthias Grob <maetugr@gmail.com>
2021-10-21 18:27:08 +0200

    ManualControl: introduce stick gesture for killing

--- src/modules/manual_control/ManualControl.cpp
+++ src/modules/manual_control/ManualControl.cpp
@@ -87,6 +87,7 @@ void ManualControl::Run()
 
 		_stick_arm_hysteresis.set_hysteresis_time_from(false, _param_com_rc_arm_hyst.get() * 1_ms);
 		_stick_disarm_hysteresis.set_hysteresis_time_from(false, _param_com_rc_arm_hyst.get() * 1_ms);
+		_stick_kill_hysteresis.set_hysteresis_time_from(false, _param_man_kill_gest_t.get() * 1_s);
 		_button_hysteresis.set_hysteresis_time_from(false, _param_com_rc_arm_hyst.get() * 1_ms);
 
 		_selector.setRcInMode(_param_com_rc_in_mode.get());
@@ -322,6 +323,7 @@ void ManualControl::Run()
 		_r_diff.reset();
 		_stick_arm_hysteresis.set_state_and_update(false, now);
 		_stick_disarm_hysteresis.set_state_and_update(false, now);
+		_stick_kill_hysteresis.set_state_and_update(false, now);
 		_button_hysteresis.set_state_and_update(false, now);
 	}
 
@@ -354,6 +356,18 @@ void ManualControl::processStickArming(const manual_control_setpoint_s &input)
 
 	if (_param_man_arm_gesture.get() && !previous_stick_disarm_hysteresis && _stick_disarm_hysteresis.get_state()) {
 		sendActionRequest(action_request_s::ACTION_DISARM, action_request_s::SOURCE_RC_STICK_GESTURE);
+	}
+
+	// Kill gesture
+	if (_param_man_kill_gest_t.get() > 0.f) {
+		const bool right_stick_lower_right = (input.x < -0.9f) && (input.y > 0.9f);
+
+		const bool previous_stick_kill_hysteresis = _stick_kill_hysteresis.get_state();
+		_stick_kill_hysteresis.set_state_and_update(left_stick_lower_left && right_stick_lower_right, input.timestamp);
+
+		if (!previous_stick_kill_hysteresis && _stick_kill_hysteresis.get_state()) {
+			sendActionRequest(action_request_s::ACTION_KILL, action_request_s::SOURCE_RC_STICK_GESTURE);
+		}
 	}
 }
 
